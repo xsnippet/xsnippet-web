44a16f7e7b1b059090ae151736629e8523b91dac
Olha Kurkaiedova <olya.kurkaedova@gmail.com>
2017-12-24 21:44:36 +0200

    Implement 'Recent snippets' page styles
    This commit adds all styles for Recent Page so now it looks according to
    design. All data were stubbed

--- src/components/RecentSnippets.jsx
+++ src/components/RecentSnippets.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
+import Title from './common/Title';
 
-const RecentSnippet = () => (
-  <div>
-    Recent Snippet page
-  </div>
+import '../styles/RecentSnippets.styl';
+
+const RecentSnippets = () => (
+  [
+    <Title title="Recent snippets" additionalClass="recent-title" key="title-recent" />,
+    <ul className="recent-snippet" key="recent-snippet">
+      <li className="recent-snippet-item">
+        <div className="recent-snippet-data">
+          <div>
+            <span className="recent-snippet-data-title">#235435, Untitles</span>
+            <span className="recent-snippet-data-lang">[ Java ]</span>
+          </div>
+          <span className="recent-snippet-data-author">By Guest</span>
+        </div>
+        <div>
+          <button className="recent-snippet-button light">Raw</button>
+          <button className="recent-snippet-button light">Download</button>
+          <button className="recent-snippet-button">Show</button>
+        </div>
+      </li>
+      <li className="recent-snippet-item">
+        <div className="recent-snippet-data">
+          <div>
+            <span className="recent-snippet-data-title">#235435, Untitles</span>
+            <span className="recent-snippet-data-lang">[ Java ]</span>
+          </div>
+          <span className="recent-snippet-data-author">By Guest</span>
+        </div>
+        <div>
+          <button className="recent-snippet-button light">Raw</button>
+          <button className="recent-snippet-button light">Download</button>
+          <button className="recent-snippet-button">Show</button>
+        </div>
+      </li>
+    </ul>,
+  ]
 );
 
-export default RecentSnippet;
+export default RecentSnippets;
