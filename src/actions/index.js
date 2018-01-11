a1aeb9a276ef519ad5f3b8657e85c5489aa19715
Olha Kurkaiedova <olya.kurkaedova@gmail.com>
2018-01-11 22:30:44 +0200

    Add pagination to Recent snippets page
    To let user navigate through existing snippets and fetch snippets
    proportinaly we have added two buttons Next and Previous
    so we can get go throught all snippets by step equal 20

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,28 @@
+import parseLinkHeader from 'parse-link-header';
+
 export const setRecentSnippets = snippets => ({
   type: 'SET_RECENT_SNIPPETS',
   snippets,
 });
 
-export const fetchRecentSnippets = dispatch => (
-  fetch('http://api.xsnippet.org/snippets')
-    .then(response => response.json())
-    .then(json => dispatch(setRecentSnippets(json)))
-);
+export const setPaginationLinks = links => ({
+  type: 'SET_PAGINATION_LINKS',
+  links,
+});
+
+export const fetchRecentSnippets = marker => (dispatch) => {
+  let qs = '';
+  if (marker) { qs = `&marker=${marker}`; }
+
+  return fetch(`http://api.xsnippet.org/snippets?limit=20${qs}`)
+    .then((response) => {
+      const links = parseLinkHeader(response.headers.get('Link'));
+
+      dispatch(setPaginationLinks(links));
+      return response.json();
+    })
+    .then(json => dispatch(setRecentSnippets(json)));
+};
 
 export const setSnippet = snippet => ({
   type: 'SET_SNIPPET',
