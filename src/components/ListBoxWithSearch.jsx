e030d56af9ce9dc155b0e39baab9b946a117bd7f
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2018-01-14 12:59:27 +0200

    ListBox item is now an object, not string
    There are few reasons behind this change, so here they are:
    
     * We are about to replace CodeMirror with Ace, and there we have a
       different set of supported syntaxes. Due to internals, it would be
       easiery for us to maintain syntaxes mode names on server side instead
       of syntaxes human names (e.g. c_cpp instead of "C and C++"). Since
       we want to separate enum value from its representational name,
       ListBox component is ought to separate these concepts.
    
     * ListBox is designed as a replacement for standard HTML <select> tag
       and the latter separates the value it will send as selected from the
       value it shows on the page. So why don't we?
    
     * The change makes component more general, which is one step closer to
       make it independent library.
    
    The important note here is that it maintains both user interfaces, just
    like <select> tag and one can pass either an array of strings or an
    array of objects (with "name" and "value" props).

--- src/components/ListBoxWithSearch.jsx
+++ src/components/ListBoxWithSearch.jsx
@@ -20,11 +20,19 @@ class ListBoxWithSearch extends React.PureComponent {
     const { searchQuery } = this.state;
     let { items } = this.props;
 
+    // Normalize items arrays so each item is always an object.
+    items = items.map((item) => {
+      if (item !== Object(item)) {
+        return { name: item, value: item };
+      }
+      return item;
+    });
+
     // Filter out only those items that match search query. If no query is
     // set, do nothing and use the entire set.
     if (searchQuery) {
       const regExp = new RegExp(regExpEscape(searchQuery), 'gi');
-      items = items.filter(item => item.match(regExp));
+      items = items.filter(item => item.name.match(regExp));
     }
 
     return (
