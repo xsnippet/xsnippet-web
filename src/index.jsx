e71b1c7991f4f7384f773ae1102e62c4c871463b
Olha Kurkaeidova <olya.kurkaedova@gmail.com>
2019-06-12 21:31:30 +0300

    [React-update] Upgrade react to 16.8.6
    Upgraded React version and removed additional component (Main). It
    seemed useless. Moved BrowserRouter to App component to keep all
    react-router components within one file

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import App from './components/App'
@@ -11,9 +10,7 @@ const store = createStore()
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   ), document.getElementById('root'),
 )
