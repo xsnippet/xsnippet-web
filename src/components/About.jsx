498401c63dea7d6b41d91e1983f9e8593668b2a3
Olha Kurkaeidova <olya.kurkaedova@gmail.com>
2019-08-18 12:05:48 +0300

    [About] Update class names on About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,89 +1,94 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import bemi from 'bemi'
+
 import Title from './common/Title'
 
-import '../styles/AboutUs.styl'
 import RomanImage from '../assets/photos/roman.jpg'
 import IhorImage from '../assets/photos/ihor.jpg'
 import OlhaImage from '../assets/photos/olha.jpg'
 import KaterynaImage from '../assets/photos/kateryna.jpg'
 
+import '../styles/AboutUs.styl'
+
+const about = bemi('about')
+
 const About = () => (
-  [
-    <div className="about-us" key="about">
-      <Title title="About" key="About title" />
-      <p className="about-us-paragraph">
+  <Fragment>
+    <div className={about.b()}>
+      <Title title="About" />
+      <p className={about.e('paragraph', 'padded')}>
         XSnippet is an open source web-service for sharing code snippets on
         the Internet. Years ago it was started as educational project, and
         nothing has changed since then. The service has been rewritten several
         times from scratch and now it consists of:
       </p>
-      <ul className="about-us-repo-list">
-        <li className="about-us-repo-item">
-          <span className="about-us-repo">xsnippet-api</span>
-          <p className="about-us-paragraph">
+      <ul className={about.e('repo-list')}>
+        <li className={about.e('repo-item')}>
+          <span className={about.e('repo')}>xsnippet-api</span>
+          <p className={about.e('paragraph')}>
             It implements a RESTful API to manage snippets and flies on top of
             asyncio/aiohttp and MongoDB;
           </p>
         </li>
-        <li className="about-us-repo-item">
-          <span className="about-us-repo">xsnippet-web</span>
-          <p className="about-us-paragraph">
+        <li className={about.e('repo-item')}>
+          <span className={about.e('repo')}>xsnippet-web</span>
+          <p className={about.e('paragraph')}>
             It provides a web-based user interface (SPA) and flies on top of
             React/Redux stack;
           </p>
         </li>
-        <li className="about-us-repo-item">
-          <span className="about-us-repo">xsnippet-web-backend</span>
-          <p className="about-us-paragraph">
+        <li className={about.e('repo-item')}>
+          <span className={about.e('repo')}>xsnippet-web-backend</span>
+          <p className={about.e('paragraph')}>
             It is a tiny app which implements what doesn’t fall under API
             description yet required by xsnippet-web.
           </p>
         </li>
       </ul>
-      <p className="about-us-paragraph">
+      <p className={about.e('paragraph')}>
         These components, and other goodies, can be found in our GitHub:
       </p>
-      <a className="about-us-github" href="http://github.com/xsnippet/">
+      <a className={about.e('github')} href="http://github.com/xsnippet/">
         http://github.com/xsnippet/
       </a>
-    </div>,
-    <div className="about-us" key="team">
-      <Title title="Our Team" key="Team title" />
-      <p className="about-us-paragraph">
+    </div>
+    <div className={about.b()}>
+      <Title title="Our Team" />
+      <p className={about.e('paragraph', 'padded')}>
         Modern XSnippet incarnation has been designed and developed by these fine people:
       </p>
-      <ul className="about-us-team-list">
-        <li className="about-us-team-item">
+      <ul className={about.e('team-list')}>
+        <li className={about.e('team-item')}>
           <img src={IhorImage} alt="Ihor Kalnytskyi" />
           <div>
-            <span className="about-us-team-member">Ihor Kalnytskyi</span>
-            <p className="about-us-paragraph small">Back-end developer</p>
+            <span className={about.e('team-member')}>Ihor Kalnytskyi</span>
+            <p className={about.e('paragraph', 'small')}>Back-end developer</p>
           </div>
         </li>
-        <li className="about-us-team-item">
+        <li className={about.e('team-item')}>
           <img src={RomanImage} alt="Roman Podoliaka" />
           <div>
-            <span className="about-us-team-member">Roman Podoliaka</span>
-            <p className="about-us-paragraph small">Back-end developer</p>
+            <span className={about.e('team-member')}>Roman Podoliaka</span>
+            <p className={about.e('paragraph', 'small')}>Back-end developer</p>
           </div>
         </li>
-        <li className="about-us-team-item">
+        <li className={about.e('team-item')}>
           <img src={OlhaImage} alt="Olha Kurkaiedova" />
           <div>
-            <span className="about-us-team-member">Olha Kurkaiedova</span>
-            <p className="about-us-paragraph small">Front-end developer</p>
+            <span className={about.e('team-member')}>Olha Kurkaiedova</span>
+            <p className={about.e('paragraph', 'small')}>Front-end developer</p>
           </div>
         </li>
-        <li className="about-us-team-item">
+        <li className={about.e('team-item')}>
           <img src={KaterynaImage} alt="Kateryna Zharikova" />
           <div>
-            <span className="about-us-team-member">Kateryna Zharikova</span>
-            <p className="about-us-paragraph small">UI Designer</p>
+            <span className={about.e('team-member')}>Kateryna Zharikova</span>
+            <p className={about.e('paragraph', 'small')}>UI Designer</p>
           </div>
         </li>
       </ul>
-    </div>,
-  ]
+    </div>
+  </Fragment>
 )
 
 export default About
--- src/components/common/Notification.jsx
+++ src/components/common/Notification.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import '../../styles/common/Notification.styl'
 
 const Notification = ({ message }) => (
   <div className="notification has-error">{message}</div>
 )
-
-Notification.propTypes = {
-  message: PropTypes.string,
-}
-
-Notification.defaultProps = {
-  message: '',
-}
 
 export default Notification
